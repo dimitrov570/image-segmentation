3c4336c7b9c031ef9dece71ebb8a348afdaed8d8
dimitrov570 <ivajlo.dimitrov99@gmail.com>
2021-01-23 18:09:44 +0100

    split into multiple modules

--- Segmentation.hs
+++ Segmentation.hs
@@ -1,103 +1,15 @@
 module Segmentation  where
 
-import Data.Word
+import Image
 import System.IO
 import Data.Char
 import System.Environment
 
-data Rgb = Rgb { red   :: Word8
-               , green :: Word8
-               , blue  :: Word8 } deriving (Show, Read, Eq)
-
-{-- instance Show Rgb where
-    show (Rgb r g b) = show r ++ " " ++ show g ++ " " ++ show b
---}
-
-data Pixel = Pixel { color   :: Rgb
-                   , cluster :: Rgb
-                   , distance  :: Double } deriving (Show,Read)
-
-data Image = Image { width   :: Int
-                   , height  :: Int
-                   , content :: [[Pixel]] } deriving (Show, Read)
-
-
-{- function is used in saveImage -}
-printCluster :: Pixel -> String
-printCluster (Pixel _ (Rgb r g b) _) = show r ++ " " ++ show g ++ " " ++ show b
-
-
-defaultCluster = (Rgb 0 0 0)
-
-{-- actually, this function multiplies any Integral number, but returns Word8 --}
-multWord8WithFrac :: (RealFrac a) => Word8 -> a -> Word8
-multWord8WithFrac w n = round (fromIntegral w * n) :: Word8
-
-clamp :: Int -> Word8
-clamp n 
-    | n > 255 = maxBound :: Word8
-    | n < 0 = 0
-    | otherwise = fromIntegral n
-
-{- Transforms list of lists of Rgb into list of Pixel -}
-rgbToPixel :: [[Rgb]] -> [Pixel]
-rgbToPixel [] = []
-rgbToPixel xs = map (\x -> Pixel x (Rgb 0 0 0) maxDist) $ concat xs
-                    where maxDist = fromIntegral (maxBound :: Int)
-
 -- imageSegmentation :: Image -> Image
 
 
-{- FUNCTION FOR SAVING IMAGE TO FILE -}
-
-saveImage :: FilePath -> Image -> IO()
-saveImage path img = do 
-                    writeFile path header
-                    appendFile path (maxColorValue ++ "\n")
-                    appendFile path $ unlines (map printCluster pixels) --lazy
-                    where header        = "P3\n" ++ show (width img) ++ " " ++ show (height img) ++ "\n"
-                          maxColorValue = show 255
-                          pixels        = concat $ content img
-
-
-{-- Д --}
-
-getWidth :: [String] -> Int
-getWidth ln = read (ln !! 0) :: Int
-
-getHeight :: [String] -> Int
-getHeight ln = read (ln !! 1) :: Int
-
-extractRow :: (Eq t, Num t) => t -> [String] -> [Pixel]
-extractRow 0 _ = []
-extractRow _ [] = []
-extractRow w lst@(r:g:b:rest) = (Pixel (Rgb (read r :: Word8) (read g :: Word8) (read b :: Word8))
-                                        defaultCluster
-                                        maxDist) : extractRow (w - 1) rest
-                            where maxDist       = fromIntegral (maxBound :: Int)
-extractRow _ _ = error "Wrong file structure!"
-
-extractPixels :: (Eq t, Num t) => Int -> t -> [String] -> [[Pixel]]
-extractPixels _ 0 _ = []
-extractPixels w h lst = extractRow w lst : extractPixels w (h - 1) (drop (w * 3) lst)
-
-parseImg :: [String] -> Image
-parseImg (f:x:y:xs) 
-    | f /= "P3" = error "Unknown format"
-    | otherwise = (Image width height pixels)
-                        where firstLineWords = (words x)
-                              width = getWidth firstLineWords
-                              height = getHeight firstLineWords
-                              pixels = extractPixels width height $ words (unlines xs)
-
 selectWithComparator :: (a -> a -> a) -> [a] -> a
 selectWithComparator cmp lst = foldl1 (\acc x -> cmp acc x) lst
-
-loadImage :: String -> IO Image
-loadImage path = do contents <- readFile path
-                    let linesOfFile = lines contents
-                        img = parseImg linesOfFile
-                    return img
 
 main = do
     args <- getArgs
